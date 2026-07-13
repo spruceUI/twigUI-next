e2ab5cc6f299323dfb59e2ebf1fc8faec121713a
Hairo R. Carela <hairocr8@gmail.com>
2026-07-13 19:59:02 -0400

    Update joypad id

--- projects/twigUI/packages/twig/package.mk
+++ projects/twigUI/packages/twig/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2023 JELOS (https://github.com/JustEnoughLinuxOS)
 
 PKG_NAME="twig"
-PKG_VERSION="af12240458a23803b3b9291f206f8d0cf5968d07"
+PKG_VERSION="6e8074872d1033353884c77d4bdc501f08ba831e"
 PKG_LICENSE="Public Domain"
 PKG_SITE="https://github.com/Hairo/spruceOS/"
 PKG_URL="https://github.com/Hairo/spruceOS/archive/${PKG_VERSION}.tar.gz"
