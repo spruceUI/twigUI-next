d964770fcfd5a9c17f7bacaa62580fdbff4e270d
Hairo R. Carela <hairocr8@gmail.com>
2026-08-30 00:12:59 -0400

    Add virtualjaguar-lr emulator

--- projects/twigUI/packages/virtual/emulators/package.mk
+++ projects/twigUI/packages/virtual/emulators/package.mk
@@ -17,6 +17,6 @@ LIBRETRO_CORES="a5200-lr ardens-lr atari800-lr bluemsx-lr cap32-lr chailove-lr c
                 beetle-supafaust-lr beetle-supergrafx-lr beetle-vb-lr beetle-wswan-lr mgba-lr mupen64plus-nx-lr neocd-lr nestopia-lr \
                 np2kai-lr o2em-lr parallel-n64-lr pcsx_rearmed-lr picodrive-lr pokemini-lr potator-lr prboom-lr prosystem-lr \
                 puae2021-lr px68k-lr quicknes-lr race-lr sameduck-lr snes9x-lr stella2014-lr swanstation-lr tgbdual-lr tic80-lr \
-                tyrquake-lr uae4arm-lr vecx-lr vice-lr yabasanshiro-lr"
+                tyrquake-lr uae4arm-lr vecx-lr vice-lr virtualjaguar-lr yabasanshiro-lr"
 
 PKG_DEPENDS_TARGET="${PKG_EMUS} ${LIBRETRO_CORES}"
