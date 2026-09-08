71d3f4531160568d53f433afb12c0c9f9cb6aa05
Hairo R. Carela <hairocr8@gmail.com>
2026-09-08 19:47:55 -0400

    Fix vulkan

--- projects/twigUI/packages/graphics/vulkan/vulkan-wsi-layer/package.mk
+++ projects/twigUI/packages/graphics/vulkan/vulkan-wsi-layer/package.mk
@@ -13,27 +13,22 @@ PKG_LONGDESC="Implements Vulkan extensions for Window System Integration inside 
 PKG_TOOLCHAIN="cmake"
 
 pre_configure_target() {
-if [ "${DEVICE}" = "RK3588" ] || [ "${DEVICE}" = "RK3326S" ] ; then
-  #BSP name?, probably can be removed when moving to mainline
-  HEAP_NAME=cma
-else
-  HEAP_NAME=linux,cma
-fi
+  HEAP_NAME=cma-uncached
 
-if [ "${ARCH}" = "aarch64" ]; then
-  INCLUDE_ARCH=arm64
-else
-  INCLUDE_ARCH=${ARCH}
-fi
+  if [ "${ARCH}" = "aarch64" ]; then
+    INCLUDE_ARCH=arm64
+  else
+    INCLUDE_ARCH=${ARCH}
+  fi
 
-PKG_CMAKE_OPTS_TARGET+=" -DVULKAN_CXX_INCLUDE=${SYSROOT_PREFIX}/usr \
-        -DBUILD_WSI_HEADLESS=OFF \
-        -DBUILD_WSI_WAYLAND=ON \
-        -DSELECT_EXTERNAL_ALLOCATOR=dma_buf_heaps \
-        -DWSIALLOC_MEMORY_HEAP_NAME=${HEAP_NAME} \
-        -DENABLE_WAYLAND_FIFO_PRESENTATION_THREAD=ON \
-        -DKERNEL_HEADER_DIR=$(get_build_dir linux)/arch/${INCLUDE_ARCH}/include \
-        -DCMAKE_POLICY_VERSION_MINIMUM=3.5"
+  PKG_CMAKE_OPTS_TARGET+=" -DVULKAN_CXX_INCLUDE=${SYSROOT_PREFIX}/usr \
+          -DBUILD_WSI_HEADLESS=OFF \
+          -DBUILD_WSI_WAYLAND=ON \
+          -DSELECT_EXTERNAL_ALLOCATOR=dma_buf_heaps \
+          -DWSIALLOC_MEMORY_HEAP_NAME=${HEAP_NAME} \
+          -DENABLE_WAYLAND_FIFO_PRESENTATION_THREAD=ON \
+          -DKERNEL_HEADER_DIR=$(get_build_dir linux)/arch/${INCLUDE_ARCH}/include \
+          -DCMAKE_POLICY_VERSION_MINIMUM=3.5"
 }
 
 makeinstall_target() {
