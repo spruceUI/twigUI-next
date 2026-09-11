b026259ce21c2367fcc2060557b7d224c45ea7b1
Hairo R. Carela <hairocr8@gmail.com>
2026-09-11 00:48:00 -0400

    Store pulse cookie in /tmp/

--- projects/twigUI/packages/audio/pulseaudio/package.mk
+++ projects/twigUI/packages/audio/pulseaudio/package.mk
@@ -77,13 +77,5 @@ post_makeinstall_target() {
 
   cp ${PKG_DIR}/config/system.pa ${INSTALL}/etc/pulse/
 
-  # Sometimes apps cannot load `libpulsecommon`. It is located in ${libdir}/pulseaudio and is not intended to be loaded directly.
-  # Application loads `libpulse.so.0`, `libpulse` has `libpulsecommon` as a dep and RPATH set to ${libdir}/pulseaudio
-  # So, if there is error loading `libpulsecommon`, check RPATH of `libpulse` and `libpulse-simple`
-  # Adding pulseaudio subdir to `/etc/ld.so.conf.d/*.conf` may be a working hack for that, but please check/fix RPATH first
+  echo $'\ncookie-file = /tmp/pulse-cookie' >> ${INSTALL}/etc/pulse/client.conf
 }
-
-# Deprecated by pipewire
-#post_install() {
-#  enable_service pulseaudio.service
-#}
